4544bb2696643997908cda12aa9eb91d69ac8570
Kilian Huegler <kilianhuegler@icloud.com>
2026-04-18 21:18:17 +0300

    Level 3 - Test 6: can be rotated left/counter-clockwise -> Changed constructor, shapes and methods for rotation functionality

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -1,23 +1,24 @@
 import {RotatingShape} from "./RotatingShape.mjs";
 
 export class Tetromino {
-  constructor(shape) {
-    this.shape = shape;
+  constructor(orientations, i = 0) {
+    this.orientations = orientations;
+    this.i = i;
   }
 
-  static T_SHAPE = new Tetromino(RotatingShape.fromString(".T.\nTTT\n..."));
+  static T_SHAPE = Tetromino.orientations(".T.\nTTT\n...", 4);
 
-  static I_SHAPE = new Tetromino(RotatingShape.fromString(".....\n.....\nIIII.\n.....\n....."));
+  static I_SHAPE = Tetromino.orientations(".....\n.....\nIIII.\n.....\n.....", 2);
 
   toString() {
-    return this.shape.toString();
+    return this.orientations[this.i].toString();
   }
 
   rotateRight() {
-    return new Tetromino(this.shape.rotateRight());
+    return new Tetromino(this.orientations, (this.i + 1) % this.orientations.length);
   }
 
   rotateLeft() {
-    return new Tetromino(this.shape.rotateLeft());
+    return new Tetromino(this.orientations, (this.i - 1 + this.orientations.length) % this.orientations.length);
   }
 }
