93b322c99d950c6769c8ab15ce8075573954ea5b
Kilian Huegler <kilianhuegler@icloud.com>
2026-04-19 17:10:39 +0300

    Level 5 - Added test structure for MovingTetrominoes tests

--- test/MovingTetrominoes.test.mjs
+++ test/MovingTetrominoes.test.mjs
@@ -5,9 +5,18 @@ import { Tetromino } from "../src/Tetromino.mjs";
 
 describe("MovingTetrominoes", () => {
   let board;
-
   beforeEach(() => {
     board = new Board(10, 6);
   });
+
+  test("a falling tetromino can be moved left", () => {});
+  test("a falling tetromino can be moved right", () => {});
+  test("a falling tetromino can be moved down", () => {});
+  test("it cannot be moved left beyond the board", () => {});
+  test("it cannot be moved right beyond the board", () => {});
+  test("it cannot be moved down beyond the board (will stop falling)", () => {});
+  test("it cannot be moved left through other blocks", () => {});
+  test("it cannot be moved right through other blocks", () => {});
+  test("it cannot be moved down through other blocks (will stop falling)", () => {});
 
 });
