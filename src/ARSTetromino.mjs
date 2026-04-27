82315b294a0222ef936d26ef8369ee266d89488e
Kilian Huegler <kilianhuegler@icloud.com>
2026-04-27 21:59:37 +0300

    Level 7 - Added new file for ARS Tetrominos

---
+++ src/ARSTetromino.mjs
@@ -0,0 +1,10 @@
+export class ARSTetromino {
+  constructor(rotations, i = 0) {
+    this.rotations = rotations;
+    this.i = i;
+  }
+  static T_SHAPE = new ARSTetromino(["TTT.\n.T..\n....\n...."]);
+  toString() {
+    return this.rotations[this.i];
+  }
+}
